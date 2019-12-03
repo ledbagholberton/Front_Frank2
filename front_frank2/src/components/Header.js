2c68d021466cd137edb10a770dd85086f87c1b43
ledbagholberton <789@holbertonschool.com>
2019-12-03 20:32:04 +0000

    Header ready

--- front_frank2/src/components/Header.js
+++ front_frank2/src/components/Header.js
@@ -1,40 +1,35 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css'
-import imgLogo from '../images/myvdo2.png'
+import 'bootstrap/dist/css/bootstrap.css';
+//import 'bootstrap/dist/css/bootstrap-theme.css';
+import imgLogo from '../images/my_full_2.png'
+import imgName from '../images/2.png'
 import './Styles/Header.css'
 
 
 class Header extends React.Component {
     render() {
 	    return(
-        <div>            
-            <header className="header">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-3">
-                            <div className="logo">
-                                <img src={imgLogo} />
-                            </div>
-                        </div>
-                        <div className="col-3">
-                            <ul className="d-flex flex-row align-items-center justify-content-start">
-                                <li className="active"><a href="/">Home</a></li>
-                                <li><a href="/about/">About myvdo</a></li>
-                            </ul>
-                        </div>
-                        <div className="col-3">
-                            <div className="logo">
-                                <a href="/" className="d-flex flex-row align-items-end justify-content-start">
-                                    <span className="logo_bars d-flex flex-row align-items-end justify-content-between"><span></span><span></span><span></span><span></span><span></span></span>
-                                    <span className="logo_text">myvdo</span>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
+        <div className="container-fluid">    
+            <div className="row">    
+                <div className="col-3 mx-auto">
+                    <img src={imgLogo} className="logo img-responsive"/>
                 </div>
-            </header>
+                <div className="col-9">
+                    <ul class="nav nav-justified">
+                        <li class="nav-item">
+                        <a class="nav-link active" href="#">Home</a>
+                        <span class="glyphicon glyphicon-music"></span>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <hr className="separator"/>
+            
         </div>
-		)
+    )
 	}
 }
 
